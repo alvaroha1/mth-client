ff31a5bd8779b3af712c0ee0615cefcd2d18102c
stevenspyramid <40496047+stevenspyramid@users.noreply.github.com>
2018-10-06 19:50:41 +0200

    Revert "Folders"

--- client/src/redux/actions.js
+++
@@ -1,9 +0,0 @@
-export const logIn = apiInfo => ({
-  type: 'GETHOUSES',
-  api: {
-    endpoint: apiInfo.endpoint,
-    method: apiInfo.method,
-    body: apiInfo.body,
-    headers: apiInfo.headers
-  }
-});
