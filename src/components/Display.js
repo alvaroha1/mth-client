0f1ea7e072f62854ac38a3add78bdec9d59173e8
alvaroha1 <alvarohernandezassens@gmail.com>
2018-10-10 13:45:23 +0200

    feat: add fetch homes working in display

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,18 +4,22 @@ import Map from './Map'
 import ItemList from './ItemList'
 import { connect } from 'react-redux'
 
-
-// const Display = this.state.bool ? Map : Results;
-
 class Display extends Component {
 	constructor(props) {
 		super(props)
+		this.props.getHomesFromDb();
 		this.state = {
 			isMapOn: this.props.isMapOn,
 		}
 	}
 	
+	getHomesFromDb = async () => {
+		await this.props.getHomesFromDb()
+		// this.setState
+	}
+
 	render() {
+		{console.log(this.state)}
 		if(this.state.isMapOn) {
 			return (
 
@@ -40,7 +44,27 @@ const mapStateToProps = (state) => ({
 	mapToggle:state.isMapOn
 })
 
-const mapDispatchToProps = () => ({
-})
+const mapDispatchToProps = (dispatch) => ({
+  getHomesFromDb: () => dispatch({
+    type: 'GET_HOMES',
+    api: {
+      endpoint: '/homes'
+    }
+  })
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Display)
+
+
+
+
+// const mapDispatchToProps = (dispatch) => ({
+//   getHobbiesFromDatabase: () => dispatch({
+//     type: 'DISCOVER',
+//     api: {
+//       endpoint: '/discover'
+//     }
+//   })
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Discover);
