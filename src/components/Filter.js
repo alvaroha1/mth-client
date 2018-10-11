e4a62f18f45ae57cb72e7ee78f734a0d31b4b5c1
alvaroha1 <alvarohernandezassens@gmail.com>
2018-10-11 14:27:08 +0200

    feature: add sliders

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,56 +1,64 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { debounce } from 'lodash.debounce'
+import Slider, { Range } from 'rc-slider'
+import 'rc-slider/assets/index.css'
+import './Filter.css'
+
+const size = {
+	0: <strong>0</strong>,
+	200: <strong>200m</strong>,
+}
+
+const price = {
+	0: <strong>0</strong>,
+	2000: <strong>2MM€</strong>,
+}
 
 class Filter extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			country:'',
-			price:'',
-			size:'',
+			price:1000 * 1000,
+			size:100,
 		}
 	}
 
-	handleSlidersChange = (e) => {
-		console.log(e);
-		// const target = event.target;
-    // const value = target.value;
-  }
+	setPrice = (value) => {
+		// console.log(value)
+		this.setState({price: value * 1000})
+		console.log(this.state)
+	}
+
+	setSize = (value) => {
+		// console.log(value)
+		this.setState({size: value})
+		console.log(this.state)
+	}
 
 	render() {
 		return (
 			<div className="Filter">
-	
-				<div className="Country">
-					<h4><strong>Country</strong></h4>
-					<div className="field has-addons">
-						<div className="control is-expanded">
-							<div className="select is-fullwidth">
-								<select name="country">
-									<option value="Spain">Spain</option>
-									<option value="Portugal">Portugal</option>
-									<option value="Italy">Italy</option>
-								</select>
-							</div>
-						</div>
-						<div className="control">
-							<button name="country" type="submit" className="button is-primary">Choose</button>
-						</div>
-					</div>
+
+				<h4><strong>Max Price</strong></h4>
+				<div className="slider">
+					<Slider min={0} max={2000} marks={price} included={false} defaultValue={1000} onChange={this.setPrice}/>
 				</div>
 
-				<div>
-					<h4><strong>Price</strong></h4>
-					<input className="slider is-fullwidth is-medium" step="1" min="0" max="100" value="50" type="range" onChange={this.handleInputChange}></input>
+				<h4><strong>Max Size</strong></h4>
+				<div className="slider">
+					<Slider min={0} max={200} marks={size} included={false} defaultValue={100} onChange={this.setSize}/>
 				</div>
-
-				<div>
-					<h4><strong>Size</strong></h4>
-					<input className="slider is-fullwidth is-medium" step="1" min="0" max="100" value="50" type="range" onChange={this.handleInputChange}></input>
-				</div>
-			
 			</div>
 		)
 	}
 }
 
-export default Filter
+const mapStateToProps = (state) => ({
+	homes: state.homes,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter)
