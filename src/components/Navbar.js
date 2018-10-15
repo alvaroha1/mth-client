7712415ccfeb531fda22659928d87c00a130df76
alvaroha1 <alvarohernandezassens@gmail.com>
2018-10-15 12:36:16 +0200

    fix: small code fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,33 +7,33 @@ class Navbar extends Component {
 				<nav className="navbar is-link" role="navigation" aria-label="main navigation">
 					<div className="navbar-brand">
 						<a className="navbar-item" href="https://bulma.io">
-							<img src="" width="112" height="28"></img>
+							<img alt="logo" src="" width="112" height="28"></img>
 						</a>
-						<a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+						<a href="/" role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
 							<span aria-hidden="true"></span>
 							<span aria-hidden="true"></span>
 						</a>
 					</div>
 					<div id="navbarBasicExample" className="navbar-menu">
 						<div className="navbar-start">
-							<a className="navbar-item">
+							<a href="/" className="navbar-item">
 								Home
 							</a>
-							<a className="navbar-item">
+							<a href="/" className="navbar-item">
 								Rates
 							</a>
 							<div className="navbar-item has-dropdown is-hoverable">
-								<a className="navbar-link">
+								<a href="/" className="navbar-link">
 									About Us
 								</a>
 								<div className="navbar-dropdown">
-									<a className="navbar-item">
+									<a href="/" className="navbar-item">
 										About MTH
 									</a>
-									<a className="navbar-item">
+									<a href="/" className="navbar-item">
 										Team
 									</a>
-									<a className="navbar-item">
+									<a href="/" className="navbar-item">
 										ContactUs
 									</a>
 								</div>
@@ -42,10 +42,10 @@ class Navbar extends Component {
 						<div className="navbar-end">
 							<div className="navbar-item">
 								<div className="buttons">
-									<a className="button is-light">
+									<a href="/" className="button is-light">
 										<strong>Sign up</strong>
 									</a>
-									<a className="button is-light">
+									<a href="/" className="button is-light">
 										<strong>Log in</strong>
 									</a>
 								</div>
