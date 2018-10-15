ec574aa9286c420ab4603e67d102c0d1f9cf08a5
alvaroha1 <alvarohernandezassens@gmail.com>
2018-10-15 11:26:44 +0200

    fix: add key in itemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,19 +2,22 @@ import React from 'react'
 import Item from './Item'
 
 function ItemList({itemList}){
-	const listItems = itemList.map((home)=>(
-		<Item url={home.url} thumbnail={home.thumbnail}
+	const listItems = itemList.map((home, i)=>(
+		<Item
+			key={i} 
+			url={home.url}
+			thumbnail={home.thumbnail}
 			pricePerSquareMeter={home.pricePerSquareMeter}
-			price={home.price} estimatedPrice={home.estimatedPrice}
+			price={home.price} 
+			estimatedPrice={home.estimatedPrice}
 		/>
-	)
-	)
+	))
+
 	return (
 		<div className="ItemList">
 			{ listItems }
 		</div>
 	)
 }
-
 
 export default ItemList
