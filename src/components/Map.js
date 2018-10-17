a88149b988bbc5c1851fe788f3380f81cbce193d
stevenspyramid <steven.vanasbeck@gmail.com>
2018-10-17 14:10:43 +0200

    refactor: removed duplicate code

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
 import ReactMapGL, {Marker} from 'react-map-gl'
 import './Map.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
-library.add(faMapMarker)
-const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3RldmVuc3B5cmFtaWQiLCJhIjoiY2puMWl4NDluM3g5aTNwcG56YWVhb293YiJ9.UpzML4DXnrPKkVdvY0IOJQ'
+
+const accesToken = 'pk.eyJ1Ijoic3RldmVuc3B5cmFtaWQiLCJhIjoiY2puMWl4NDluM3g5aTNwcG56YWVhb293YiJ9.UpzML4DXnrPKkVdvY0IOJQ'
 
 class Map extends Component {
 	constructor(props){
@@ -34,45 +31,28 @@ class Map extends Component {
 	}
 
 	render() {
+		const positiveNegative = (num)=>num>0 ? 'success' : 'danger'
 		const list = this.props.itemList.map((home, i)=>{
-			if(home.estimatedPricePercentageDifference>0){
-				return(
-					<Marker key={i} latitude={home.latitude} longitude={home.longitude}>
-						<span className="tag is-success">
-							<div className="tooltip">
-								{home.estimatedPricePercentageDifference}%
-								<span className="tooltiptext">
-									<a href={home.url}>	
-										<img src={home.thumbnail}/>
-									</a>		
-								</span>
-							</div>
-						</span>
-					</Marker>
-				)
-			}
-			if(home.estimatedPricePercentageDifference<0){
-				return(
-					<Marker key={i} latitude={home.latitude} longitude={home.longitude}>
-						<span className="tag is-danger">
-							<div className="tooltip">
-								{home.estimatedPricePercentageDifference}%
-								<span className="tooltiptext">
-									<a href={home.url}>	
-										<img className="img" src={home.thumbnail}/>
-									</a>							
-								</span>
-							</div>
-						</span>
-					</Marker>
-				)
-			}
+			return(
+				<Marker key={i} latitude={home.latitude} longitude={home.longitude}>
+					<span className={`tag is-${positiveNegative(home.estimatedPricePercentageDifference)}`}>
+						<div className="tooltip">
+							{home.estimatedPricePercentageDifference}%
+							<span className="tooltiptext">
+								<a href={home.url}>	
+									<img src={home.thumbnail}/>
+								</a>		
+							</span>
+						</div>
+					</span>
+				</Marker>
+			)
 		}
 		)
 
 		return (
 			<ReactMapGL
-				mapboxApiAccessToken={'pk.eyJ1Ijoic3RldmVuc3B5cmFtaWQiLCJhIjoiY2puMWl4NDluM3g5aTNwcG56YWVhb293YiJ9.UpzML4DXnrPKkVdvY0IOJQ'}
+				mapboxApiAccessToken={accesToken}
 				{...this.state.viewport}
 				onViewportChange={(viewport) =>{
 					this.refreshWidth()
