63f9747decc17b0d999d535d4f20cc499eb84c7b
Peter Schindler <petertasschindler@gmail.com>
2018-10-18 12:42:21 +0200

    feat: add queryparams to request

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,17 +1,24 @@
 import { RSAA } from 'redux-api-middleware'
+import qs from 'qs'
+const serverAddress = 'http://192.168.1.163:3001'
 
 export const mapToggle = data => ({
 	type: 'MAP_TOGGLE',
 	bool: data,
 })
 
-export const getFilterHomes = () => ({
-	[RSAA]: {
-		endpoint: 'http://192.168.1.163:3001/homes?estimatedPricePercentageDifference=-10&price%5B0%5D=0&price%5B1%5D=1000000&size%5B0%5D=0&size%5B1%5D=200&country=it&city=&estimatedPrice%5B0%5D=0&estimatedPrice%5B1%5D=2000000&page=200&centerLongitude=8.849993&centerLatitude=44.427967&radius=1000',
-		method: 'GET',
-		types: [ 'FILTERED_HOMES_REQUEST', 'FILTERED_HOMES_SUCCESS', 'FILTERED_HOMES_FAILURE' ]
-	},
-})
+export const getFilterHomes = (queryParameters) => {
+	const urlParameters = qs.stringify(queryParameters)
+	const endpoint = `${serverAddress}/homes?${urlParameters}`
+	console.log(endpoint);
+	return {
+		[RSAA]: {
+			endpoint: endpoint,
+			method: 'GET',
+			types: [ 'FILTERED_HOMES_REQUEST', 'FILTERED_HOMES_SUCCESS', 'FILTERED_HOMES_FAILURE' ]
+		},
+	}
+}
 
 export const filterHomes = apiInfo => ({
 	type: 'FILTER_HOMES',
